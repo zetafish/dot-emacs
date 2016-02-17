92c079a3447ad22100990a623d189099459ac9b7
Endymion Kasanardjo <endy.kasanardjo@bluemango.nl>
2016-02-17 11:09:46 +0100

    add zz-org

---
+++ modules/zz-org.el
@@ -0,0 +1,31 @@
+;;; -*- lexical-binding: t -*-
+;;; zz-org.el
+
+;; Copyright (C) 2015 Endymion Kasanardjo
+
+;; Author: Endymion Kasanardjo <zetafish@gmail.com>
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+(setq org-todo-keywords
+      '((sequence "TODO" "IN PROGRESS" "DONE")))
+
+
+(setq org-use-fast-todo-selection t)
+(setq org-treat-S-cursor-todo-selection-as-state-change nil)
+(setq org-clock-persist 'history)
+
+(org-clock-persistence-insinuate)
+
+(provide 'zz-org)
