2d2d8e955da9e6be93f32168ee60759e78c5f640
Endymion Kasanardjo <zetafish@gmail.com>
2022-07-15 21:24:42 +0200

    update

--- init.el
+++ init.el
@@ -12,7 +12,6 @@
 (scroll-bar-mode -1)
 (setq ring-bell-function 'ignore)
 
-
 ;; Backup settings.
 (setq backup-by-copying t)
 (setq backup-directory-alist '(("." . "~/.saves")))
@@ -27,19 +26,18 @@
 (which-function-mode)       ; Show current function in modeline.
 (display-time)              ; Show time in modeline.
 (display-battery-mode)      ; Show battery in modeline.
-
 (show-paren-mode 1)         ; Highlight matching braces.
+(global-linum-mode)         ; Show line numbers
 
-;;(set-frame-font "Fira Mono-13" nil t)
-(set-frame-font "Monaco-15" nil t)
-;; (set-frame-font "Mono-12" nil t)
-;; (set-frame-font "Courier New-13" nil t)
-;; (set-frame-font "Andale Mono-13" nil t)
-;; (set-frame-font "courier-13" nil t)
-;; (set-frame-font "Monofur-13" nil t)
-;; (set-frame-font "Hack-12" nil t)
-;; (set-frame-font "Sauce Code Powerline-12" nil t)
-;; (set-frame-font "Inconsolata-13" nil t)
+;;(set-frame-font "Monaco-14" nil t)
+(set-frame-font "JetBrains Mono-15" nil t)
+;;(set-frame-font "Courier New-13" nil t)
+;;(set-frame-font "Andale Mono-15" nil t)
+;;(set-frame-font "courier-13" nil t)
+;;(set-frame-font "Monofur-13" nil t)
+;;(set-frame-font "Hack-14" nil t)
+;;(set-frame-font "Inconsolata" nil t)
+;;(set-frame-font "Roboto Mono for Powerline-15" nil t)
 
 (global-set-key (kbd "C-c <tab>") 'indent-buffer)
 (global-set-key (kbd "M-j") 'join-line)
@@ -51,26 +49,17 @@
 ;; Automatically reload files when they change
 (global-auto-revert-mode t)
 
-(defun present-on ()
-  "Enter presentation mode."
-  (interactive)
-  (set-face-attribute 'default nil :height 200))
-
-(defun present-off ()
-  "Leave presentation mode."
-  (interactive)
-  (set-face-attribute 'default nil :height 120))
-
 (package-initialize)
 
 (require 'package)
 
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+;;(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 ;;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+;;(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 
 (add-to-list 'load-path "~/.emacs.d/elisp")
+(add-to-list 'load-path "~/.emacs.d/elisp/typescript.el")
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -87,19 +76,28 @@
 (use-package color-theme-sanityinc-solarized :defer t)
 (use-package dracula-theme :defer t)
 (use-package doom-themes :defer t)
+(use-package ubuntu-theme :defer t)
+(use-package gotham-theme :defer t)
 
+;;(load-theme 'ubuntu)
+;;(load-theme 'gotham)
 ;;(load-theme 'grandshell t)
+;;(load-theme 'doom-acario-dark)
 ;;(load-theme 'dracula t)
 ;;(load-theme 'doom-sourcerer t)
-(load-theme 'doom-tomorrow-night t)
+;;(load-theme 'doom-tomorrow-night t)
 ;;(load-theme 'doom-tomorrow-day t)
+;;(load-theme 'doom-city-lights)
 ;;(load-theme 'doom-vibrant  t)
-;;(load-theme 'doom-one t)
+(load-theme 'doom-one t)
 ;;(load-theme 'leuven t)
 
-(use-package diminish
-  :config
-  (diminish 'eldoc-mode))
+(use-package diminish)
+
+(use-package company
+  :diminish)
+
+(global-company-mode)
 
 (use-package highlight-parentheses
   :commands highlight-parentheses-mode
@@ -107,7 +105,7 @@
   (highlight-parentheses-mode 1)
   (add-hook 'emacs-lisp-mode-hook 'highlight-parentheses-mode)
   (add-hook 'clojure-mode-hook 'highlight-parentheses-mode)
-  :diminish highlight-parentheses-mode)
+  :diminish)
 
 (use-package paredit
  :demand t
@@ -116,31 +114,9 @@
  (add-hook 'lisp-mode-hook 'paredit-mode)
  (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
  (add-hook 'clojure-mode-hook 'paredit-mode)
- :diminish paredit-mode)
+ :diminish)
 
-(use-package paxedit
-  :commands paxedit-mode
-  :config
-  (add-hook 'emacs-lisp-mode-hook 'enable-paxedit-mode)
-  (add-hook 'clojure-mode-hook 'enabled-paxedit-mode)
-  :bind (:map paxedit-mode-map
-              ("M-<right>" . paxedit-transpose-forward)
-              ("M-<left>" . paxedit-transpose-backward)
-              ("M-<up>" . paxedit-backward-up)
-              ("M-<down>" . paxedit-backward-end)
-              ("M-b" . paxedit-previous-symbol)
-              ("M-f" . paxedit-next-symbol)
-              ("C-&" . paxedit-kill)
-              ("C-%" . paxedit-copy)
-              ("C-*" . paxedit-delete)
-              ("C-^" . paxedit-sexp-raise)
-              ;; Symbol backward/forward kill
-              ("C-w" . paxedit-backward-kill)
-              ("M-w" . paxedit-forward-kill)
-              ;; Symbol manipulation
-              ("M-u" . paxedit-symbol-change-case)
-              ("C-@" . paxedit-symbol-copy)
-              ("C-#" . paxedit-symbol-kill)))
+(use-package origami)
 
 (use-package nyan-mode
   :config
@@ -154,19 +130,16 @@
   :commands magit-status
   :bind (("C-x g" . magit-status)))
 
-(use-package gitignore-mode)
-
 (use-package diff-hl
   :config
   (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh))
-
-;; (use-package gist)
 
 (use-package git-gutter-fringe
   :config
   (global-git-gutter-mode t)
   :diminish git-gutter-mode)
 
+;;; https://elpa.gnu.org/packages/counsel.html
 (use-package counsel)
 
 (use-package ivy
@@ -219,41 +192,28 @@
 ;;   (volatile-highlights-mode t)
 ;;   :diminish volatile-highlights-mode)
 
+(setq mode-require-final-newline nil)
+
 (use-package ethan-wspace
   :demand t
   :commands ethan-wspace-mode
   :config
   (global-ethan-wspace-mode t)
   :bind ("C-c c" . ethan-wspace-clean-all)
-  :diminish ethan-wspace-mode)
+  :diminish)
 
 (use-package rainbow-mode)
 
 (use-package rainbow-delimiters)
-
-;;(add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
-
-;;(add-hook 'clojure-mode-hook 'eldoc-mode)
 
 (when (memq window-system '(mac ns))
   (use-package exec-path-from-shell
     :ensure t
     :config (exec-path-from-shell-initialize)))
 
-;; (use-package python)
-
-;; (use-package yaml-mode)
-
-;; ;; (use-package elpy
-;; ;;   :init
-;; ;;   (elpy-enable))
-
-;; ;; (use-package py-yapf
-;; ;;   :config)
-
-;; (use-package sphinx-doc
-;;   :config
-;;   (add-hook 'python-mode-hook 'sphinx-doc-mode))
+(use-package hl-todo
+  :config
+  (global-hl-todo-mode t))
 
 (use-package rjsx-mode)
 
@@ -271,18 +231,74 @@
   :config
   (add-hook 'prettier-js-mode-hook 'add-node-modules-path))
 
+(use-package lsp-mode
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :hook ((clojure-mode . lsp)
+         (clojurescript-mode . lsp)
+         (clojurec-mode . lsp)
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+(use-package lsp-ui :commands lsp-ui-mode
+  :config
+  (setq lsp-ui-sideline-show-diagnostics t)
+  (setq lsp-ui-sideline-show-code-actions nil)
+  (setq lsp-ui-sideline-update-mode 'point))
+
+
+(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
+
+(use-package which-key
+    :config
+    (which-key-mode))
+
+;; [M-x] lsp-install-server [RET] clojure-lsp
+
+(with-eval-after-load 'lsp-mode
+  ;; :global/:workspace/:file
+  ;;(lsp-lens-mode)
+  (setq lsp-modeline-diagnostics-scope :workspace)
+  (setq lsp-headerline-breadcrumb-enable nil))
+
 (use-package clj-refactor
   :commands clj-refactor-mode
-  :config
-  (cljr-add-keybindings-with-prefix "C-c C-m")
+  :init
   (add-hook 'clojure-mode-hook 'clj-refactor-mode))
 
+(use-package flycheck-clj-kondo
+  :ensure t)
 
+(load-library "cljstyle-mode.el")
+
+(use-package zprint-mode
+  :config
+  ;;(add-hook 'clojure-mode-hook 'zprint-mode)
+  ;;(add-hook 'clojurescript-mode-hook 'zprint-mode)
+  )
+
+(use-package clojure-mode
+  :ensure t
+  :config
+  (add-hook 'clojure-mode-hook 'cljstyle-mode)
+  (require 'flycheck-clj-kondo))
+
+(use-package kaocha-runner
+  :init
+  (bind-keys :prefix-map ar-emacs-kaocha-prefix-map
+             :prefix "C-c k"
+             ("t" . kaocha-runner-run-test-at-point)
+             ("r" . kaocha-runner-run-tests)
+             ("a" . kaocha-runner-run-all-tests)
+             ("w" . kaocha-runner-show-warnings)
+             ("h" . kaocha-runner-hide-windows)))
 
 (use-package cider
   :commands cider-mode
   :config
-  (setq cider-clojure-cli-global-options "-R:dev:test")
+  ;;(setq cider-clojure-cli-global-options "-R:dev:test:build")
   ;;(setq cider-clojure-cli-global-options "-R:dev:test -C:dev:test")
   :bind (("C-c C-t t" . cider-test-run-test)
          ("C-c C-t n" . cider-test-run-ns-tests)
@@ -294,74 +310,32 @@
     '(sayid-setup-package)))
 
 (use-package projectile
+  :diminish
   :ensure t
   :config
   (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
   (projectile-mode +1))
-
-(use-package clojure-mode
-  :commands clojure
-  :config
-  (setq clojure-indent-style :always-align)
-  (add-hook 'clojure-mode-hook
-            (lambda ()
-              (rainbow-delimiters-mode-enable)
-              (define-clojure-indent
-                (context 2) (GET 2) (POST 2) (DELETE 2) (PATCH 2))))
-  ;;(add-hook 'clojure-mode-hook #'aggressive-indent-mode)
-  )
-
-;; (use-package flycheck-clojure
-;;   :config
-;;   (eval-after-load 'flycheck '(flycheck-clojure-setup))
-;;   (add-hook 'after-init-hook 'global-flycheck-mode))
-
-;; (use-package flycheck-clojure)
-
-;; ;; (use-package flycheck-pos-tip
-;; ;;   :config
-;; ;;   (eval-after-load 'flycheck '(setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
 
 (use-package flycheck
   :ensure t
   :config
   (add-hook 'after-init-hook #'global-flycheck-mode))
 
-(use-package flycheck-joker
-  :demand t)
+;; (use-package flycheck-joker
+;;   :demand t)
 
-(use-package flycheck-clj-kondo
-  :demand t)
+;; (use-package flycheck-clj-kondo
+;;   :demand t)
 
-(dolist (checkers '((clj-kondo-clj . clojure-joker)
-                    (clj-kondo-cljs . clojurescript-joker)
-                    (clj-kondo-cljc . clojure-joker)
-                    (clj-kondo-edn . edn-joker)))
-  (flycheck-add-next-checker (car checkers) (cons 'error (cdr checkers))))
+;; (dolist (checkers '((clj-kondo-clj . clojure-joker)
+;;                     (clj-kondo-cljs . clojurescript-joker)
+;;                     (clj-kondo-cljc . clojure-joker)
+;;                     (clj-kondo-edn . edn-joker)))
+;;   (flycheck-add-next-checker (car checkers) (cons 'error (cdr checkers))))
 
 (use-package clojure-mode-extra-font-locking)
 
 (use-package docker-compose-mode)
-
-;; (use-package slime
-;;   :config
-;;   (setq inferior-lisp-program "/usr/local/bin/sbcl")
-;;   (setq slime-contribs '(slime-fancy)))
-
-
-;; (use-package jsonnet-mode)
-
-;; ;;(use-package company-jedi)
-;; (use-package python-docstring)
-;; (use-package sphinx-doc)
-;; (use-package python
-;;   :config (setq python-shell-interpreter "python"
-;;                 python-shell-interpreter-args "--profile=dev -i")
-;;   :init
-;;   (add-hook 'python-mode-hook (lambda ()
-;;                                   (auto-complete-mode -1)
-;;                                 (python-docstring-mode)
-;;                                 (sphinx-doc-mode))))
 
 (use-package blacken
   :init
@@ -371,7 +345,7 @@
 
 (use-package yafolding)
 
-;;(use-package restclient)
+(use-package typescript-mode)
 
 (setq-default json-indent-level 2)
 (setq-default js-indent-level 2)
